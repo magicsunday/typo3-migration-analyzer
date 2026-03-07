b9b4f4d49a7b575833471e0a22f10694a12d70b9
Rico Sonntag <rico.sonntag@netresearch.de>
2026-03-07 07:31:13 +0000

    Initial Symfony 7.2 project setup with TYPO3 dependencies

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PER-CS2.0' => true,
+        '@PER-CS2.0:risky' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arguments', 'arrays', 'match', 'parameters'],
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => false,
+            'import_functions' => false,
+        ],
+    ])
+    ->setFinder($finder);
