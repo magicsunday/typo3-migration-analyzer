dac14f496e8c140e9d06853d38bc129ec599c82b
Rico Sonntag <mail@ricosonntag.de>
2026-03-07 14:24:46 +0100

    Build targets automatically restart containers after building

--- Make/build.mk
+++ Make/build.mk
@@ -6,9 +6,11 @@
 
 .PHONY: build rebuild
 
-build: .logo ## Builds/Updates the used docker images.
-	# Add --no-cache to force rebuild
+build: .logo ## Builds/Updates the used docker images and restarts containers.
 	$(COMPOSE_BIN) build --pull
+	$(COMPOSE_BIN) up -d
 
-rebuild: .logo ## Forces a complete rebuild of all docker images (no cache).
+rebuild: .logo ## Forces a complete rebuild of all docker images (no cache) and restarts containers.
+	$(COMPOSE_BIN) down -v
 	$(COMPOSE_BIN) build --pull --no-cache
+	$(COMPOSE_BIN) up -d
