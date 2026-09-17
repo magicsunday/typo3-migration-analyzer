1479c0f4580d9739570e840818e4f241ed425585
Rico Sonntag <rico.sonntag@netresearch.de>
2026-09-17 18:13:35 +0200

    GH-11: Add the missing @return tag to ControlCharacterSanitizer::strip()
    Every other non-void method with @param tags in this codebase pairs
    them with @return; this one didn't.
    
    Signed-off-by: Rico Sonntag <rico.sonntag@netresearch.de>

--- src/Support/ControlCharacterSanitizer.php
+++ src/Support/ControlCharacterSanitizer.php
@@ -30,6 +30,8 @@ final class ControlCharacterSanitizer
      *
      * @param string $value       The value to sanitize.
      * @param string $replacement The string each control character is replaced with.
+     *
+     * @return string The sanitized value.
      */
     public static function strip(string $value, string $replacement = ''): string
     {
